e90e0aaead13d34d234c5f06fa8553cb0b567731
Simone Vellei <simone.vellei@inim.biz>
2015-09-18 16:47:32 +0200

    fix

--- launch.sh
+++ launch.sh
@@ -2,20 +2,16 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/restful-server/:$PWD/tcc/lib/tcc/:$PWD/sundown/
 
 
-PREFIX=""
 COMMAND="./tcc/bin/tcc -run  -I restful-server/ -Isundown/src/ -Isundown/html/ -Lrestful-server/ -Lsundown/ -lfossa -lsundown restful-server/restful_server.c"
 
 if [ $1 ];  then
-	PREFIX="sudo -u $1 "
-	COMMAND="$PREFIX$COMMAND"
 	
 	while true; do
 		eval $COMMAND
-		sleep 1
 	done
 
 else
-	`$COMMAND`
+	eval $COMMAND
 fi
 
 
