9ceed365582dcf454cd5e0143b28fff1cb62019d
Simone Vellei <simone.vellei@inim.biz>
2015-09-18 16:27:35 +0200

    fix for server side

--- launch.sh
+++ launch.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/restful-server/:$PWD/tcc/lib/tcc/:$PWD/sundown/
 
-./tcc/bin/tcc -run  -I restful-server/ -Isundown/src/ -Isundown/html/ -Lrestful-server/ -Lsundown/ -lfossa -lsundown restful-server/restful_server.c 
+
+PREFIX=""
+COMMAND="./tcc/bin/tcc -run  -I restful-server/ -Isundown/src/ -Isundown/html/ -Lrestful-server/ -Lsundown/ -lfossa -lsundown restful-server/restful_server.c"
+
+if [ $1 ];  then
+	PREFIX="sudo -u $1 "
+	COMMAND="$PREFIX$COMMAND"
+	
+	while true; do
+		eval $COMMAND
+		sleep 1
+	done
+
+else
+	`$COMMAND`
+fi
+
+
+
+
